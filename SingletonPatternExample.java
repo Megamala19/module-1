a8c69c41025270b9e7819a3e8a0e776c17acba39
Megamala19 <mightymegu@gmail.com>
2026-07-01 13:54:29 +0530

    Create SingletonPatternExample.java

---
+++ SingletonPatternExample.java
@@ -0,0 +1,28 @@
+class Logger {
+    private static Logger instance;
+
+    private Logger() {
+    }
+
+    public static Logger getInstance() {
+        if (instance == null) {
+            instance = new Logger();
+        }
+        return instance;
+    }
+
+    public void log(String msg) {
+        System.out.println("LOG: " + msg);
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Logger l1 = Logger.getInstance();
+        Logger l2 = Logger.getInstance();
+
+        l1.log("First Message");
+
+        System.out.println(l1 == l2);
+    }
+}
